1fc8ad8c5f6a0a01d71436a52cf4ec5cd424d422
Brandon Li <40838203+PulseBeat02@users.noreply.github.com>
2025-01-28 19:00:00 -0700

    Bumped JDA

--- mcav-jda/build.gradle.kts
+++ mcav-jda/build.gradle.kts
@@ -4,13 +4,13 @@ plugins {
 
 dependencies {
     // project dependencies
-    api("net.dv8tion:JDA:5.5.1")
+    api("net.dv8tion:JDA:5.6.0")
 
     // provided
     compileOnlyApi(project(":mcav-common"))
 
     // testing
-    testImplementation("net.dv8tion:JDA:5.5.1")
+    testImplementation("net.dv8tion:JDA:5.6.0")
     testImplementation(project(":mcav-common"))
 }
 
