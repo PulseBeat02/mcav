b11733fc47d765b5aafc09687a3d7849d75a9b0f
Brandon Li <brandonli2006ma@gmail.com>
2025-07-21 17:42:28 -0400

    Forgot File

---
+++ sandbox/mod/src/main/java/me/brandonli/command/HelloCommand.java
@@ -0,0 +1,39 @@
+/*
+ * This file is part of mcav, a media playback library for Java
+ * Copyright (C) Brandon Li <https://brandonli.me/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package me.brandonli.command;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
+
+public final class HelloCommand {
+
+  public static void register(final CommandDispatcher<ServerCommandSource> dispatcher) {
+    dispatcher.register(CommandManager.literal("hello").executes(HelloCommand::execute));
+  }
+
+  private static int execute(final CommandContext<ServerCommandSource> context) {
+    final ServerCommandSource source = context.getSource();
+    source.sendFeedback(() -> Text.literal("Hello!"), false);
+    return 1;
+  }
+
+  private HelloCommand() {}
+}
